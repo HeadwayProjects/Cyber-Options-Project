879571074a86121fa6e998d3d6ac7567b8c2bbc8
HeadwayProjects <hdsprojects23@gmail.com>
2023-05-10 15:20:38 +0530

    [Krishna] < Courses loading changes >

--- cyber_options/cyber_options_app/models.py
+++ cyber_options/cyber_options_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    course_name = models.CharField(max_length=50)
+    course_price = models.IntegerField()
+    course_description = models.TextField()
+    category = models.CharField(max_length=150)
